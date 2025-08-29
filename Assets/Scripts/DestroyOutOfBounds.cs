3c53d8ee023642d8cfa0de637664f4dda93eeff2
Mila Data <camilamorales.it@gmail.com>
2025-08-29 14:00:03 -0400

    feat: game interface (instead of logs) we have a counter

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,10 +6,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
     private float sideBound = 30;
+
+    // Game score
+    private GameManager gameManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -22,17 +26,17 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if(transform.position.x > sideBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if(transform.position.x < -sideBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
